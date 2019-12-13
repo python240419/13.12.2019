fa9abeb2d46d75994c9608d26e093a38bf4c4f99
python240419 <51232248+python240419@users.noreply.github.com>
2019-12-13 10:00:20 +0200

    Create wrapper.py

---
+++ wrapper.py
@@ -0,0 +1,19 @@
+# decorator - function which calls another function
+def wrapper(your_function):
+    print("before f2 ===========")
+
+    your_function()
+
+    print("after f2 ===========")
+
+def hello_func():
+    print("hello world!")
+
+
+def math_x():
+    x = 10
+    print(x)
+
+wrapper(hello_func)
+wrapper(math_x())
+
